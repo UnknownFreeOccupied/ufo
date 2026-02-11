aaf2315d136dd036018c4652f04a492e3846a36f
Daniel Duberg <danielduberg@gmail.com>
2026-02-11 21:17:45 +0100

    Updated to CMAKE_INSTALL_INCLUDEDIR

--- external/wgpu_native/wgpu_native.cmake
+++ external/wgpu_native/wgpu_native.cmake
@@ -140,5 +140,5 @@ install(
 # # NOTE: It should be the same files in both release and debug
 install(DIRECTORY ${WGPU_DIR_RELEASE}/include
 	COMPONENT Compute
-	DESTINATION ${CMAKE_INSTALL_PREFIX}
+	DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
